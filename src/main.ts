4f2608c7e03d3f3f8abab0ddbc36660737f0e298
Rolf Erik Lekang <me@rolflekang.com>
2018-11-24 11:52:46 +0100

    fix: Use dynamic loading of commands

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
+import path from 'path';
 import args from '@relekang/args';
 
 import { hasConfig, createConfig } from './config';
-import * as add from './commands/add';
-import * as list from './commands/list';
-import * as next from './commands/next';
-import * as pri from './commands/pri';
-import * as complete from './commands/complete';
 
 args({
   name: 'todo',
-  commands: { add, list, next, pri, complete },
+  commandsPath: path.resolve(__dirname, './commands'),
   defaultCommand: 'next',
   needsSetup: () => !hasConfig,
   setup: createConfig,
